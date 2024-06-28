75e9fc4d9c53acb0160598a95c19d2bc65344a15
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-06-28 19:09:10 +0530

    add get host

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -120,7 +120,17 @@ func (repo *DBRepo) PostSettings(w http.ResponseWriter, r *http.Request) {
 
 // AllHosts displays list of all hosts
 func (repo *DBRepo) AllHosts(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "hosts", nil, nil)
+
+	hosts, err := repo.DB.AllHosts()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	vars := make(jet.VarMap)
+	vars.Set("hosts", hosts)
+
+	err = helpers.RenderPage(w, r, "hosts", vars, nil)
 	if err != nil {
 		printTemplateError(w, err)
 	}
@@ -133,7 +143,12 @@ func (repo *DBRepo) Host(w http.ResponseWriter, r *http.Request) {
 	var h models.Host
 
 	if id > 0 {
-		// get the host from the database
+		host, err := repo.DB.GetHostByID(id)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		h = host
 	}
 	vars := make(jet.VarMap)
 	vars.Set("host", h)
@@ -149,33 +164,46 @@ func (repo *DBRepo) PostHost(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 
 	var h models.Host
-	var hostID int
 
 	if id > 0 {
 		// get the host from the database
-	} else {
-		h.HostName = r.Form.Get("host_name")
-		h.CanonicalName = r.Form.Get("canonical_name")
-		h.URL = r.Form.Get("url")
-		h.IP = r.Form.Get("ip")
-		h.IPV6 = r.Form.Get("ipv6")
-		h.Location = r.Form.Get("location")
-		h.OS = r.Form.Get("os")
-		active, _ := strconv.Atoi(r.Form.Get("active"))
-		h.Active = active
+		host, err := repo.DB.GetHostByID(id)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		h = host
+	}
 
+	h.HostName = r.Form.Get("host_name")
+	h.CanonicalName = r.Form.Get("canonical_name")
+	h.URL = r.Form.Get("url")
+	h.IP = r.Form.Get("ip")
+	h.IPV6 = r.Form.Get("ipv6")
+	h.Location = r.Form.Get("location")
+	h.OS = r.Form.Get("os")
+	active, _ := strconv.Atoi(r.Form.Get("active"))
+	h.Active = active
+
+	if id > 0 {
+		err := repo.DB.UpdateHost(h)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
 		newID, err := repo.DB.InsertHost(h)
 		if err != nil {
 			log.Println(err)
 			helpers.ServerError(w, r, err)
 			return
 		}
-		hostID = newID
+		h.ID = newID
 	}
 
 	repo.App.Session.Put(r.Context(), "flash", "Changes Saved")
 
-	http.Redirect(w, r, fmt.Sprintf("/admin/host/%d", hostID), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/admin/host/%d", h.ID), http.StatusSeeOther)
 }
 
 // AllUsers lists all admin users
--- internal/repository/dbrepo/host_postgresql.go
+++ internal/repository/dbrepo/host_postgresql.go
@@ -27,3 +27,105 @@ func (m *postgresDBRepo) InsertHost(h models.Host) (int, error) {
 
 	return newID, nil
 }
+
+func (m *postgresDBRepo) GetHostByID(id int) (models.Host, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select id, host_name, canonical_name, url, ip, ipv6, location, os, active, created_at, updated_at from hosts where id = $1`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var h models.Host
+
+	err := row.Scan(
+		&h.ID,
+		&h.HostName,
+		&h.CanonicalName,
+		&h.URL,
+		&h.IP,
+		&h.IPV6,
+		&h.Location,
+		&h.OS,
+		&h.Active,
+		&h.CreatedAt,
+		&h.UpdatedAt,
+	)
+
+	if err != nil {
+		return h, err
+	}
+
+	return h, nil
+}
+
+func (m *postgresDBRepo) UpdateHost(h models.Host) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `update hosts set host_name = $1, canonical_name = $2, url = $3, ip = $4, ipv6 = $5, location = $6, os = $7, active = $8, updated_at = $9 where id = $10`
+
+	_, err := m.DB.ExecContext(ctx, query,
+		h.HostName,
+		h.CanonicalName,
+		h.URL,
+		h.IP,
+		h.IPV6,
+		h.Location,
+		h.OS,
+		h.Active,
+		time.Now(),
+		h.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *postgresDBRepo) AllHosts() ([]models.Host, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select id, host_name, canonical_name, url, ip, ipv6, location, os, active, created_at, updated_at from hosts order by host_name`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var hosts []models.Host
+
+	for rows.Next() {
+		var h models.Host
+		err = rows.Scan(
+			&h.ID,
+			&h.HostName,
+			&h.CanonicalName,
+			&h.URL,
+			&h.IP,
+			&h.IPV6,
+			&h.Location,
+			&h.OS,
+			&h.Active,
+			&h.CreatedAt,
+			&h.UpdatedAt,
+		)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		hosts = append(hosts, h)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return hosts, nil
+}
